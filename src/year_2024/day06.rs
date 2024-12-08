7e07c47cdfbbfd5a313c14a195e41a06a1180006
Simon Siegler <simon.siegler@gmail.com>
2024-12-09 00:58:26 +0100

    Solve year 2024 day 06 part 2

--- src/year_2024/day06.rs
+++ src/year_2024/day06.rs
@@ -1,6 +1,7 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
+use std::hash::Hash;
 
-#[derive(Copy, Clone, Debug, PartialEq)]
+#[derive(Copy, Clone, Debug, PartialEq, Hash, Eq)]
 enum Direction {
     Up,
     Right,
@@ -19,95 +20,106 @@ impl Direction {
             Left => Up,
         }
     }
+
+    fn advance(&self, (row, column): (isize, isize)) -> (isize, isize) {
+        match self {
+            Up => (row - 1, column),
+            Right => (row, column + 1),
+            Down => (row + 1, column),
+            Left => (row, column - 1),
+        }
+    }
 }
 
+#[derive(Clone, PartialEq)]
+enum Tile {
+    Empty,
+    Obstacle,
+}
+
+#[derive(Clone)]
 pub struct Input {
-    width: usize,
-    height: usize,
-    obstructions_positions: HashSet<(usize, usize)>,
-    guard_start_position: (usize, usize),
+    tiles: HashMap<(isize, isize), Tile>,
+    guard_start_position: (isize, isize),
     guard_start_direction: Direction,
 }
 
+fn step(
+    position: (isize, isize),
+    direction: Direction,
+    tiles: &HashMap<(isize, isize), Tile>,
+) -> Option<((isize, isize), Direction)> {
+    let next_position = direction.advance(position);
+    match tiles.get(&next_position) {
+        None => None,
+        Some(Tile::Empty) => Some((next_position, direction)),
+        Some(Tile::Obstacle) => step(position, direction.turn_right(), tiles),
+    }
+}
+
 impl Input {
-    fn advance(
-        &self,
-        (row, column): &(usize, usize),
-        direction: &Direction,
-    ) -> Option<(usize, usize)> {
-        let (row, column, direction) = (*row, *column, *direction);
-        match direction {
-            Up if row > 0 => Some((row - 1, column)),
-            Right if column < self.width - 1 => Some((row, column + 1)),
-            Down if row < self.height - 1 => Some((row + 1, column)),
-            Left if column > 0 => Some((row, column - 1)),
-            _ => None,
-        }
+    fn guard_positions(&self) -> HashSet<(isize, isize)> {
+        self.patrol()
+            .map(|(position, _)| position)
+            .collect::<HashSet<_>>()
     }
 
-    fn patrol(&self) -> impl Iterator<Item = ((usize, usize), Direction)> + use<'_> {
+    fn patrol(&self) -> impl Iterator<Item = ((isize, isize), Direction)> + use<'_> {
         std::iter::successors(
             Some((self.guard_start_position, self.guard_start_direction)),
-            |(position, direction)| {
-                self.advance(position, direction).and_then(|advanced| {
-                    if self.obstructions_positions.contains(&advanced) {
-                        let turned = direction.turn_right();
-                        self.advance(position, &turned)
-                            .map(|position| (position, turned))
-                    } else {
-                        Some((advanced, *direction))
-                    }
-                })
-            },
+            |(position, direction)| step(*position, *direction, &self.tiles),
         )
+    }
+
+    fn count_cycles(&mut self) -> usize {
+        let mut obstacles = self.guard_positions();
+        obstacles.remove(&self.guard_start_position);
+        obstacles
+            .iter()
+            .filter(|obstacle| {
+                self.tiles.insert(**obstacle, Tile::Obstacle);
+                let has_cycle = {
+                    let fast = self.patrol().step_by(2);
+                    let slow = self.patrol();
+                    fast.zip(slow).skip(1).any(|(fast, slow)| fast == slow)
+                };
+                self.tiles.insert(**obstacle, Tile::Empty);
+                has_cycle
+            })
+            .count()
     }
 }
 
 pub fn part1(input: &Input) -> usize {
-    input
-        .patrol()
-        .map(|(position, _)| position)
-        .collect::<HashSet<_>>()
-        .len()
+    input.guard_positions().len()
 }
 
-pub fn part2(_input: &Input) -> String {
-    "not yet done".to_string()
+pub fn part2(input: &Input) -> usize {
+    let mut clone: Input = input.clone();
+    clone.count_cycles()
 }
 
 pub fn parse(input: &str) -> Input {
-    let height = input.lines().count();
-    let width = input.find('\n').expect("Multiple lines");
     let mut guard: Option<(usize, usize, Direction)> = None;
-    let mut obstructions_positions = HashSet::new();
+    let mut tiles = HashMap::new();
     input.lines().enumerate().for_each(|(row, line)| {
-        line.chars()
-            .enumerate()
-            .for_each(|(column, char)| match char {
-                '#' => {
-                    obstructions_positions.insert((row, column));
-                }
+        line.chars().enumerate().for_each(|(column, char)| {
+            let tile = match char {
+                '#' => Tile::Obstacle,
                 '^' => {
                     guard.replace((row, column, Up));
+                    Tile::Empty
                 }
-                '>' => {
-                    guard.replace((row, column, Right));
-                }
-                'v' => {
-                    guard.replace((row, column, Down));
-                }
-                '<' => {
-                    guard.replace((row, column, Left));
-                }
-                _ => {}
-            })
+                _ => Tile::Empty,
+            };
+            tiles.insert((row as isize, column as isize), tile);
+        })
     });
-    let (guard_start_position, guard_start_direction) =
-        guard.map(|(r, c, d)| ((r, c), d)).expect("Guard on map");
+    let (guard_start_position, guard_start_direction) = guard
+        .map(|(r, c, d)| ((r as isize, c as isize), d))
+        .expect("Guard on map");
     Input {
-        width,
-        height,
-        obstructions_positions,
+        tiles,
         guard_start_position,
         guard_start_direction,
     }
@@ -134,9 +146,15 @@ mod tests {
         let input = parse(EXAMPLE_INPUT);
         assert_eq!(input.guard_start_direction, Up);
         assert_eq!(input.guard_start_position, (6, 4));
-        assert_eq!(input.height, 10);
-        assert_eq!(input.width, 10);
-        assert_eq!(input.obstructions_positions.len(), 8);
+        assert_eq!(input.tiles.len(), 100);
+        assert_eq!(
+            input
+                .tiles
+                .iter()
+                .filter(|(_, tile)| { **tile == Tile::Obstacle })
+                .count(),
+            8
+        );
     }
 
     #[test]
@@ -165,5 +183,9 @@ mod tests {
     #[test]
     fn solves_part1() {
         assert_eq!(part1(&parse(EXAMPLE_INPUT)), 41);
+    }
+    #[test]
+    fn solves_part2() {
+        assert_eq!(part2(&parse(EXAMPLE_INPUT)), 6);
     }
 }
