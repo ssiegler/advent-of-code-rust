014b710bc01c3c321f6011761e74f1d2a3be561b
Simon Siegler <simon.siegler@gmail.com>
2024-12-16 11:29:11 +0100

    Solve year 2024 day 11 part 2

--- src/year_2024/day11.rs
+++ src/year_2024/day11.rs
@@ -1,64 +1,77 @@
 use itertools::{iterate, Itertools};
+use std::collections::HashMap;
 
-type Input = Vec<u64>;
+type Input = HashMap<u64, usize>;
 
 pub fn part1(input: &Input) -> usize {
-    iterate(input.clone(), |stones| blink(stones.as_slice()))
+    iterate(input.clone(), blink)
         .nth(25)
         .expect("stones after 25 blinks")
-        .len()
+        .values()
+        .sum()
 }
 
-pub fn part2(_input: &Input) -> usize {
-    0
+pub fn part2(input: &Input) -> usize {
+    iterate(input.clone(), blink)
+        .nth(75)
+        .expect("stones after 75 blinks")
+        .values()
+        .sum()
 }
 
 pub fn parse(input: &str) -> Input {
     input
         .split_whitespace()
         .map(|stone| stone.parse::<u64>().expect("engraved number"))
-        .collect_vec()
+        .counts()
 }
 
-fn blink(input: &[u64]) -> Input {
-    input
-        .iter()
-        .flat_map(|stone| match *stone {
-            0 => vec![1],
-            1..10 => vec![stone.checked_mul(2024).expect("u64 to be big enough")],
-            10..100 => vec![*stone / 10, *stone % 10],
-            100..1000 => vec![stone.checked_mul(2024).expect("u64 to be big enough")],
-            1000..10000 => vec![*stone / 100, *stone % 100],
-            10000..100000 => vec![stone.checked_mul(2024).expect("u64 to be big enough")],
-            100000..1000000 => vec![*stone / 1000, *stone % 1000],
-            1000000..10000000 => vec![stone.checked_mul(2024).expect("u64 to be big enough")],
-            10000000..100000000 => vec![*stone / 10000, *stone % 10000],
-            100000000..1000000000 => vec![stone.checked_mul(2024).expect("u64 to be big enough")],
-            1000000000..10000000000 => vec![*stone / 100000, *stone % 100000],
-            10000000000..100000000000 => {
-                vec![stone.checked_mul(2024).expect("u64 to be big enough")]
-            }
-            100000000000..1000000000000 => vec![*stone / 1000000, *stone % 1000000],
-            1000000000000..10000000000000 => {
-                vec![stone.checked_mul(2024).expect("u64 to be big enough")]
-            }
-            10000000000000..100000000000000 => vec![*stone / 10000000, *stone % 10000000],
-            100000000000000..1000000000000000 => {
-                vec![stone.checked_mul(2024).expect("u64 to be big enough")]
-            }
-            1000000000000000..10000000000000000 => vec![*stone / 100000000, *stone % 100000000],
-            10000000000000000..100000000000000000 => {
-                vec![stone.checked_mul(2024).expect("u64 to be big enough")]
-            }
-            100000000000000000..1000000000000000000 => {
-                vec![*stone / 1000000000, *stone % 1000000000]
-            }
-            1000000000000000000..10000000000000000000 => {
-                vec![stone.checked_mul(2024).expect("u64 to be big enough")]
-            }
-            10000000000000000000..=u64::MAX => vec![*stone / 10000000000, *stone % 10000000000],
-        })
-        .collect_vec()
+fn blink(input: &Input) -> Input {
+    let mut stones = HashMap::new();
+    for (stone, times) in input {
+        for stone in stone_rules(stone) {
+            *stones.entry(stone).or_insert(0) += times
+        }
+    }
+    stones
+}
+
+fn stone_rules(stone: &u64) -> Vec<u64> {
+    match *stone {
+        0 => vec![1],
+        1..10 => vec![stone.checked_mul(2024).expect("u64 to be big enough")],
+        10..100 => vec![*stone / 10, *stone % 10],
+        100..1000 => vec![stone.checked_mul(2024).expect("u64 to be big enough")],
+        1000..10000 => vec![*stone / 100, *stone % 100],
+        10000..100000 => vec![stone.checked_mul(2024).expect("u64 to be big enough")],
+        100000..1000000 => vec![*stone / 1000, *stone % 1000],
+        1000000..10000000 => vec![stone.checked_mul(2024).expect("u64 to be big enough")],
+        10000000..100000000 => vec![*stone / 10000, *stone % 10000],
+        100000000..1000000000 => vec![stone.checked_mul(2024).expect("u64 to be big enough")],
+        1000000000..10000000000 => vec![*stone / 100000, *stone % 100000],
+        10000000000..100000000000 => {
+            vec![stone.checked_mul(2024).expect("u64 to be big enough")]
+        }
+        100000000000..1000000000000 => vec![*stone / 1000000, *stone % 1000000],
+        1000000000000..10000000000000 => {
+            vec![stone.checked_mul(2024).expect("u64 to be big enough")]
+        }
+        10000000000000..100000000000000 => vec![*stone / 10000000, *stone % 10000000],
+        100000000000000..1000000000000000 => {
+            vec![stone.checked_mul(2024).expect("u64 to be big enough")]
+        }
+        1000000000000000..10000000000000000 => vec![*stone / 100000000, *stone % 100000000],
+        10000000000000000..100000000000000000 => {
+            vec![stone.checked_mul(2024).expect("u64 to be big enough")]
+        }
+        100000000000000000..1000000000000000000 => {
+            vec![*stone / 1000000000, *stone % 1000000000]
+        }
+        1000000000000000000..10000000000000000000 => {
+            vec![stone.checked_mul(2024).expect("u64 to be big enough")]
+        }
+        10000000000000000000..=u64::MAX => vec![*stone / 10000000000, *stone % 10000000000],
+    }
 }
 
 #[cfg(test)]
@@ -82,7 +95,10 @@ mod tests {
     #[case(&[1036288, 7, 2, 20, 24, 4048, 1, 4048, 8096, 28, 67, 60, 32],
            &[2097446912,14168,4048,2,0,2,4,40,48,2024,40,48,80,96,2,8,6,7,6,0,3,2])]
     fn blinks(#[case] input: &[u64], #[case] output: &[u64]) {
-        assert_eq!(blink(input), output);
+        assert_eq!(
+            blink(&input.iter().cloned().counts()),
+            output.iter().cloned().counts()
+        );
     }
 
     #[test]
